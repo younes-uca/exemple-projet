189d66ed53067bca92fc9e6f8345b366f6cc77c5
younes-uca <y.zouani@uca.ma>
2022-04-16 13:07:07 +0200

    prob of profile fixing

--- backend/src/main/java/com/ird/faa/FaaApplication.java
+++ backend/src/main/java/com/ird/faa/FaaApplication.java
@@ -33,7 +33,7 @@ return ctx;
 public CommandLineRunner demo(UserService userService, RoleService roleService
 ) {
 return (args) -> {
-if(false){
+if(true){
     Map<String,String> etats=new HashMap<>();
     etats.put("Initialisé","initialise");
     etats.put("En cours","encours");
